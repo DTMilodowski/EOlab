d851ee3426431c9ab49b1b7aa88d5f86fa5d78f7
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-04-22 11:45:56 +0100

    prepare_EOlab_layers.py

---
+++ src/prepare_EOlab_layers.py
@@ -0,0 +1,16 @@
+import matplotlib as mpl
+import matplotlib.cm as cm
+
+def convert_array_to_rgb(array, cmap, ulim, llim):
+  norm = mpl.colors.Normalize(vmin=llim, vmax=ulim)
+  
+  rows, cols = array.shape
+  rgb_array = np.zeros((rows,cols,3),dtype=np.uint8)*np.nan
+  for i in range(0,rows):
+    for j in range(0,cols):
+      if np.isfinite(array[i,j]):
+        rgb_array[i,j,:]= cm.ScalarMappable(norm=norm,cmap=cmap).to_rgba(array[i,j])
+      else:
+        rgb_array[i,j,:] = np.asarray([255,0,255]) # check this
+  
+  return rgb_array
