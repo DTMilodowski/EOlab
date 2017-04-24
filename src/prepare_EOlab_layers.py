b6380adad714d5d1129313d5d423b4fb247abd58
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-04-24 10:40:13 +0100

    function to write EO lab data layer

--- src/prepare_EOlab_layers.py
+++ src/prepare_EOlab_layers.py
@@ -34,37 +34,73 @@ def load_NetCDF(NetCDF_file):
 # Convert a python array with float variables into a three-band RGB array with the colours specified
 # according to a given colormap and upper and lower limits to the colormap 
 def convert_array_to_rgb(array, cmap, ulim, llim):
-  norm = mpl.colors.Normalize(vmin=llim, vmax=ulim)
+    norm = mpl.colors.Normalize(vmin=llim, vmax=ulim)
   
-  rows, cols = array.shape
-  rgb_array = np.zeros((rows,cols,3),dtype=np.uint8)*np.nan
-  for i in range(0,rows):
-    for j in range(0,cols):
-      if np.isfinite(array[i,j]):
-        rgb_array[i,j,:]= cm.ScalarMappable(norm=norm,cmap=cmap).to_rgba(array[i,j])
-      else:
-        rgb_array[i,j,:] = np.asarray([255,0,255]) # check this
+    rows, cols = array.shape
+    rgb_array = np.zeros((rows,cols,3),dtype=np.uint8)*np.nan
+    for i in range(0,rows):
+      for j in range(0,cols):
+        if np.isfinite(array[i,j]):
+          rgb_array[i,j,:]= cm.ScalarMappable(norm=norm,cmap=cmap).to_rgba(array[i,j])
+        else:
+          rgb_array[i,j,:] = np.asarray([255,0,255]) # check this
   
-  return rgb_array
+    return rgb_array
 
+def write_array_to_data_layer_GeoTiff(array,geoTrans, OUTFILE_prefix, EPSG_CODE='4326'):
+    N_bands = 1
+    NRows = 0
+    NCols = 0
 
-def write_array_to_data_layer_GeoTiff(array,<details>, OUTFILE, EPSG_CODE='4326'):
+    # Get array dimensions and flip so that it plots in the correct orientation on GIS platforms
+    if len(array.shape) < 2: 
+        print 'array has less than two dimensions! Unable to write to raster'
+        sys.exit(1)  
+    elif len(array.shape) == 2:
+        (NRows,NCols) = array.shape
+        array = np.flipud(array)
+    elif len(array.shape) == 3:
+        (NRows,NCols,NBands) = array.shape
+        for i in range(0,NBands):
+            array[:,:,i] = np.flipud(array[:,:,i])
+    else:
+        print 'array has too many dimensions! Unable to write to raster'
+        sys.exit(1)  
+    
+        
+    # Write GeoTiff
+    driver = gdal.GetDriverByName('GTiff')
+    driver.Register()
 
-    # first set all the relevant geospatial information
-    dataset_ds = driver.Create( 'temp.tif', NCols, NRows, NBands, gdal.GDT_Float32 )
-    dataset_ds.SetGeoTransform( [ XMinimum, DataResX, 0, YMinimum, 0, DataResY ] )
+    # set all the relevant geospatial information
+    dataset = driver.Create( OUTFILE_prefix+'_data.tif', NCols, NRows, NBands, gdal.GDT_Float32 )
+    dataset.SetGeoTransform( geoTrans )
     srs = osr.SpatialReference()
-    srs.SetWellKnownGeogCS( 'EPSG:'+EPSG_CODE_INIT )
+    srs.SetWellKnownGeogCS( 'EPSG:'+EPSG_CODE )
     dataset_ds.SetProjection( srs.ExportToWkt() )
     # write array
-    dataset_ds.GetRasterBand(1).SetNoDataValue( -9999 )
-    dataset_ds.GetRasterBand(1).WriteArray( Ctot_u )
-    dataset_ds = None
-    # now use gdalwarp to reproject 
-    os.system("gdalwarp -t_srs EPSG:" + EPSG_CODE_TARGET + " -srcnodata -9999 -dstnodata -9999 temp.tif " + OUTFILE)
-    os.system("rm temp.tif")    
-
+    dataset.GetRasterBand(1).SetNoDataValue( -9999 )
+    dataset.GetRasterBand(1).WriteArray( array )
+    dataset = None
     return 0
 
 def write_array_to_display_layer_GeoTiff(array,<details>):
+
+    # Write GeoTiff
+    driver = gdal.GetDriverByName('GTiff')
+    driver.Register()
+
+    # set all the relevant geospatial information
+    dataset = driver.Create( 'temp.tif', NCols, NRows, NBands, gdal.GDT_Float32 )
+    dataset.SetGeoTransform( geoTrans )
+    srs = osr.SpatialReference()
+    srs.SetWellKnownGeogCS( 'EPSG:'+EPSG_CODE )
+    dataset_ds.SetProjection( srs.ExportToWkt() )
+    # write array
+    dataset.GetRasterBand(1).SetNoDataValue( -9999 )
+    dataset.GetRasterBand(1).WriteArray( array )
+    dataset = None
+    # now use gdalwarp to reproject 
+    os.system("gdalwarp -t_srs EPSG:" + EPSG_CODE_TARGET + " -srcnodata -9999 -dstnodata -9999 temp.tif " + OUTFILE)
+    os.system("rm temp.tif")    
     return 0
