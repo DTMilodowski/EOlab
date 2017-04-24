6ed874baa353713422377cc2202fa5d03f6a69aa
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-04-24 15:36:39 +0100

    fixed axis labels

--- src/potentialAGB_app.py
+++ src/potentialAGB_app.py
@@ -15,7 +15,7 @@ vars = ['AGBobs','AGBpot','AGBreg']
 cmaps = ['YlGn','YlGn','bwr_r']
 ulims = [230.,230.,100.]
 llims = [0.,0.,-100.]
-axis_labels = ['AGB$_{obs}$ / Mg(C) ha$^{-1}', 'AGB$_{potential}$ / Mg(C) ha$^{-1}', 'AGB$_{potential} - $AGB$_{obs}$ / Mg(C) ha$^{-1}']
+axis_labels = ['AGB$_{obs}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ - AGB$_{obs}$ / Mg(C) ha$^{-1}$']
 
 for vv in range(0,len(vars)):
     print vars[vv]
@@ -30,14 +30,14 @@ for vv in range(0,len(vars)):
     #EO.write_array_to_data_layer_GeoTiff(dataset.variables[vars[vv]],geoTrans, file_prefix)
 
     EO.write_array_to_display_layer_GeoTiff(dataset.variables[vars[vv]], geoTrans, file_prefix, cmaps[vv], ulims[vv], llims[vv])
-    EO.plot_legend(cmaps[vv],ulims[vv],llims[vv],axis_labels[vv], OUTFILE_prefix)
+    EO.plot_legend(cmaps[vv],ulims[vv],llims[vv],axis_labels[vv], file_prefix)
 
 
 # create additional layer which indicates potential biomass but thresholds at zero
 cmap='YlGnBu'
 ulim = 100
 llim = 0
-axis_label = 'reforestation potential / Mg(C) ha$^{-1}'
+axis_label = 'reforestation potential / Mg(C) ha$^{-1}$'
 
 if 'tropics_reforest_potential_data.tif' in os.listdir(SAVEDIR):
     os.system("rm %s" % ( 'tropics_reforest_potential_data.tif'))
