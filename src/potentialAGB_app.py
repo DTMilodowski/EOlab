57be202d91cf702bd48ad5852db9b04dbf7950da
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-04-24 13:02:38 +0100

    updated library reqs

--- src/potentialAGB_app.py
+++ src/potentialAGB_app.py
@@ -1,3 +1,13 @@
 import numpy as np
+
 import matplotlib as mpl
 import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
+import prepare_EOlab_layers as EO
+
+NetCDF_file = '/home/dmilodow/DataStore_GCEL/AGB/AGBregpot.nc'
+dataset,geoTrans = EO.load_NetCDF(NetCDF_file)
+
+vars = ['AGBobs','AGBpot','AGBreg']
+
