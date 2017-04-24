d8bc8db127583bd10612d0299f6b29c43a168fe8
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-04-24 13:23:26 +0100

    data layers being created successfully

--- src/potentialAGB_app.py
+++ src/potentialAGB_app.py
@@ -7,7 +7,13 @@ import matplotlib.pyplot as plt
 import prepare_EOlab_layers as EO
 
 NetCDF_file = '/home/dmilodow/DataStore_GCEL/AGB/AGBregpot.nc'
+SAVEDIR = './'
 dataset,geoTrans = EO.load_NetCDF(NetCDF_file)
 
 vars = ['AGBobs','AGBpot','AGBreg']
 
+for vv in range(0,len(vars)):
+    print vars[vv]
+    file_prefix = SAVEDIR + 'tropics_' + vars[vv]
+
+    EO.write_array_to_data_layer_GeoTiff(dataset.variables[vars[vv]],geoTrans, file_prefix)
