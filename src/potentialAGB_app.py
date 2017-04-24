c40579ad8d24ce6692286c86c89b9786e9c6fdfd
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-04-24 14:04:47 +0100

    remove existing files in save directory

--- src/potentialAGB_app.py
+++ src/potentialAGB_app.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 import matplotlib as mpl
 import matplotlib.cm as cm
@@ -12,11 +13,19 @@ dataset,geoTrans = EO.load_NetCDF(NetCDF_file)
 
 vars = ['AGBobs','AGBpot','AGBreg']
 cmaps = ['YlGnBu','YlGnBu','seismic_r']
+ulims = [230.,230.,100.]
+llims = [0.,0.,-100.]
 
 for vv in range(0,len(vars)):
     print vars[vv]
     file_prefix = SAVEDIR + 'tropics_' + vars[vv]
 
+    # delete existing dataset if present
+    if 'tropics_'+vars[vv]'_data.tif' in os.listdir(SAVEDIR):
+        !rm 'tropics_'+vars[vv]'_data.tif' 
+    if 'tropics_'+vars[vv]'_display.tif' in os.listdir(SAVEDIR):
+        !rm 'tropics_'+vars[vv]'_display.tif' 
+
     #EO.write_array_to_data_layer_GeoTiff(dataset.variables[vars[vv]],geoTrans, file_prefix)
 
-    EO.write_array_to_display_layer_GeoTiff(dataset.variables[vars[vv]],geoTrans, file_prefix, cmap, ulim, llim,)
+    EO.write_array_to_display_layer_GeoTiff(dataset.variables[vars[vv]], geoTrans, file_prefix, cmaps[vv], ulims[vv], llims[vv])
