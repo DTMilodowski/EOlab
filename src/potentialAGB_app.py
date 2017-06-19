138ad526f6f22c0e06428c1868322b42a22148ea
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-06-19 13:39:41 +0100

    updates to potential biomass app

--- src/potentialAGB_app.py
+++ src/potentialAGB_app.py
@@ -9,13 +9,16 @@ import prepare_EOlab_layers as EO
 
 NetCDF_file = '/home/dmilodow/DataStore_GCEL/AGB/AGBregpot.nc'
 SAVEDIR = './'
-dataset,geoTrans = EO.load_NetCDF(NetCDF_file)
+ds,geoTrans = EO.load_NetCDF(NetCDF_file)
+dataset, geoTrans = EO.resample_dataset(ds,geoTrans,vars,resampling_scalar)
 
+resampling_scalar = 3.
 vars = ['AGBobs','AGBpot','AGBreg']
-cmaps = ['YlGn','YlGn','bwr_r']
+cmaps = ['YlGn','YlGn','PRGn']
 ulims = [230.,230.,100.]
 llims = [0.,0.,-100.]
-axis_labels = ['AGB$_{obs}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ - AGB$_{obs}$ / Mg(C) ha$^{-1}$']
+#axis_labels = ['AGB$_{obs}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ - AGB$_{obs}$ / Mg(C) ha$^{-1}$']
+axis_labels = ['AGB$_{obs}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ / Mg(C) ha$^{-1}$', 'AGB deficit / Mg(C) ha$^{-1}$']
 
 for vv in range(0,len(vars)):
     print vars[vv]
@@ -29,10 +32,10 @@ for vv in range(0,len(vars)):
 
     #EO.write_array_to_data_layer_GeoTiff(dataset.variables[vars[vv]],geoTrans, file_prefix)
 
-    EO.write_array_to_display_layer_GeoTiff(dataset.variables[vars[vv]], geoTrans, file_prefix, cmaps[vv], ulims[vv], llims[vv])
+    EO.write_array_to_display_layer_GeoTiff(dataset[vars[vv]], geoTrans, file_prefix, cmaps[vv], ulims[vv], llims[vv])
     EO.plot_legend(cmaps[vv],ulims[vv],llims[vv],axis_labels[vv], file_prefix)
 
-
+"""
 # create additional layer which indicates potential biomass but thresholds at zero
 cmap='YlGnBu'
 ulim = 100
@@ -44,5 +47,6 @@ if 'tropics_reforest_potential_data.tif' in os.listdir(SAVEDIR):
 if 'tropics_reforest_potential_display.tif' in os.listdir(SAVEDIR):
     os.system("rm %s" % ('tropics_reforest_potential_display.tif'))
 
-EO.write_array_to_display_layer_GeoTiff(dataset.variables['AGBreg'], geoTrans, 'tropics_reforest_potential', cmap, ulim, llim)
+EO.write_array_to_display_layer_GeoTiff(dataset['AGBreg'], geoTrans, 'tropics_reforest_potential', cmap, ulim, llim)
 EO.plot_legend(cmap, ulim,llim,axis_label, 'tropics_reforest_potential')
+"""
