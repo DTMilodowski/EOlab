ee15e7499a1fe28ed66d89311dcac9b1becd6376
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-06-20 22:16:21 +0100

    updating display layer file so that now the layers are plotting the correct way up again after changes to functions that it calls

--- src/prepare_EOlab_layers.py
+++ src/prepare_EOlab_layers.py
@@ -178,12 +178,45 @@ def write_array_to_data_layer_GeoTiff(array,geoTrans, OUTFILE_prefix, EPSG_CODE=
 
 # This function is similar to before, except that now it writes two GeoTIFFs - a data layer and a
 # display layer.  For the moment, this can only accept a 2D input array
-def write_array_to_display_layer_GeoTiff(array, geoTrans, OUTFILE_prefix, cmap, ulim, llim, EPSG_CODE_DATA='4326', EPSG_CODE_DISPLAY='3857'):
+def write_array_to_display_layer_GeoTiff(array, geoTrans, OUTFILE_prefix, cmap, ulim, llim, EPSG_CODE_DATA='4326', EPSG_CODE_DISPLAY='3857', north_up=True):
 
     NBands = 1
     NBands_RGB = 3
     NRows = 0
     NCols = 0
+
+    if north_up:
+        # for north_up array, need the n-s resolution (element 5) to be negative
+        if geoTrans[5]>0:
+            geoTrans[5]*=-1
+            geoTrans[3] = geoTrans[3]-(array.shape[0]+1.)*geoTrans[5]
+        # Get array dimensions and flip so that it plots in the correct orientation on GIS platforms
+        if len(array.shape) < 2: 
+            print 'array has less than two dimensions! Unable to write to raster'
+            sys.exit(1)  
+        elif len(array.shape) == 2:
+            (NRows,NCols) = array.shape
+            array = np.flipud(array)
+        else:
+            print 'array has too many dimensions! Unable to write to raster'
+            sys.exit(1)  
+
+    else:
+        # for north_up array, need the n-s resolution (element 5) to be positive
+        if geoTrans[5]<0:
+            geoTrans[5]*=-1
+            geoTrans[3] = geoTrans[3]-(array.shape[0]+1.)*geoTrans[5]
+        # Get array dimensions and flip so that it plots in the correct orientation on GIS platforms
+        if len(array.shape) < 2: 
+            print 'array has less than two dimensions! Unable to write to raster'
+            sys.exit(1)  
+        elif len(array.shape) == 2:
+            (NRows,NCols) = array.shape
+            array = np.flipud(array)
+        else:
+            print 'array has too many dimensions! Unable to write to raster'
+            sys.exit(1)  
+    
 
     # Get array dimensions and flip so that it plots in the correct orientation on GIS platforms
     if len(array.shape) < 2: 
