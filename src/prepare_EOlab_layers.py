144a82210c25ddc8d031d1ecfdc557237a3af0fd
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-06-21 12:01:19 +0100

    major efficiency saving on converting to rgb array avoiding nested loops

--- src/prepare_EOlab_layers.py
+++ src/prepare_EOlab_layers.py
@@ -101,7 +101,12 @@ def load_GeoTIFF_band_and_georeferencing(File,band_number=1):
 # according to a given colormap and upper and lower limits to the colormap 
 def convert_array_to_rgb(array, cmap, ulim, llim):
     norm = mpl.colors.Normalize(vmin=llim, vmax=ulim)
-  
+    rgb_array= cm.ScalarMappable(norm=norm,cmap=cmap).to_rgba(array)[:,:,:-1]
+    mask = ~np.isfinite(array)
+    rgb_array[mask,0]=255
+    rgb_array[mask,1]=0
+    rgb_array[mask,2]=255
+    """  
     rows, cols = array.shape
     rgb_array = np.zeros((rows,cols,3),dtype=np.uint8)*np.nan
     for i in range(0,rows):
@@ -111,7 +116,7 @@ def convert_array_to_rgb(array, cmap, ulim, llim):
                 rgb_array[i,j,:]*=255
             else:
                 rgb_array[i,j,:] = np.asarray([255,0,255]) # check this
-  
+    """
     return rgb_array
 
 # Function to write an EO lab data layer from an array
