865a9f208e9545173f0ed9efa292696bd1aae834
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-10-16 15:56:38 +0100

    updating refs to kenya

--- src/potentialAGB_Kenya_app.py
+++ src/potentialAGB_Kenya_app.py
@@ -11,49 +11,35 @@ import prepare_EOlab_layers as EO
 sys.path.append('/home/dmilodow/DataStore_DTM/FOREST2020/PotentialBiomass/src')
 import geospatial_utility_tools as geo
 
-NetCDF_file = '/home/dmilodow/DataStore_GCEL/AGB/AGBregpot.nc'
-SAVEDIR = './'
+DATADIR = '/disk/scratch/local.2/kenya_PFB/'
+SAVEDIR = '/home/dmilodow/DataStore_DTM/EOlaboratory/EOlab/KenyaPotentialAGB/'
+site = 'kenya'
 ds,geoTrans = EO.load_NetCDF(NetCDF_file,lat_var = 'lat', lon_var = 'lon')
 resampling_scalar = 3.
-vars = ['AGBobs','AGBpot','AGBreg']
+
+#vars = ['AGBobs','AGBpot','AGBreg']
+
 dataset, geoTrans = EO.resample_dataset(ds,geoTrans,vars,resampling_scalar)
 
-cmaps = ['YlGn','YlGn','PRGn']
+cmaps = ['viridis','viridis','PRGn']
 ulims = [230.,230.,100.]
 llims = [0.,0.,-100.]
-#axis_labels = ['AGB$_{obs}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ - AGB$_{obs}$ / Mg(C) ha$^{-1}$']
 axis_labels = ['AGB$_{obs}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ / Mg(C) ha$^{-1}$', 'AGB deficit / Mg(C) ha$^{-1}$']
 
 for vv in range(0,len(vars)):
     print vars[vv]
-    file_prefix = SAVEDIR + 'tropics_' + vars[vv]
+    file_prefix = SAVEDIR + site + '_' + vars[vv]
 
     # delete existing dataset if present
-    if 'tropics_'+vars[vv]+'_data.tif' in os.listdir(SAVEDIR):
-        os.system("rm %s" % ('tropics_'+vars[vv]+'_data.tif'))
-    if 'tropics_'+vars[vv]+'_display.tif' in os.listdir(SAVEDIR):
-        os.system("rm %s" % ('tropics_'+vars[vv]+'_display.tif'))
+    if site+'_'+vars[vv]+'_data.tif' in os.listdir(SAVEDIR):
+        os.system("rm %s" % (site+'_'+vars[vv]+'_data.tif'))
+    if site+'_'+vars[vv]+'_display.tif' in os.listdir(SAVEDIR):
+        os.system("rm %s" % (site+'_'+vars[vv]+'_display.tif'))
 
     #EO.write_array_to_data_layer_GeoTiff(dataset.variables[vars[vv]],geoTrans, file_prefix)
 
     EO.write_array_to_display_layer_GeoTiff(dataset[vars[vv]], geoTrans, file_prefix, cmaps[vv], ulims[vv], llims[vv])
     EO.plot_legend(cmaps[vv],ulims[vv],llims[vv],axis_labels[vv], file_prefix)
-
-"""
-# create additional layer which indicates potential biomass but thresholds at zero
-cmap='YlGnBu'
-ulim = 100
-llim = 0
-axis_label = 'reforestation potential / Mg(C) ha$^{-1}$'
-
-if 'tropics_reforest_potential_data.tif' in os.listdir(SAVEDIR):
-    os.system("rm %s" % ( 'tropics_reforest_potential_data.tif'))
-if 'tropics_reforest_potential_display.tif' in os.listdir(SAVEDIR):
-    os.system("rm %s" % ('tropics_reforest_potential_display.tif'))
-
-EO.write_array_to_display_layer_GeoTiff(dataset['AGBreg'], geoTrans, 'tropics_reforest_potential', cmap, ulim, llim)
-EO.plot_legend(cmap, ulim,llim,axis_label, 'tropics_reforest_potential')
-"""
 
 
 # Now write the quantitative display layers to file.  These probide the 
@@ -65,7 +51,7 @@ areas = geo.calculate_cell_area_array(latitude,longitude, area_scalar = 1./10.**
 # loop through the variables, multiplying by cell areas to give values in Mg
 for vv in range(0,len(vars)):
     print vars[vv]
-    file_prefix = SAVEDIR + 'tropics_' + vars[vv] + '_total_data'
+    file_prefix = SAVEDIR + site + '_' + vars[vv] + '_total_data'
 
     out_array = dataset[vars[vv]] * areas
     out_array[dataset[vars[vv]]==-9999]=-9999  # not sure why np.asarray step is needed but gets the job done
@@ -76,5 +62,5 @@ for vv in range(0,len(vars)):
 # nodata values
 areas_out = areas.copy()
 areas_out[np.asarray(dataset[vars[0]])==-9999] = -9999
-area_file_prefix = SAVEDIR + 'tropics_cell_areas_data'
+area_file_prefix = SAVEDIR + site
 EO.write_array_to_data_layer_GeoTiff(areas_out, geoTrans, area_file_prefix)
