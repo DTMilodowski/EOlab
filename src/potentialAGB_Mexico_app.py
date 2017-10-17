6cd14dced1c16dd60fb581416983fa1c0a817efb
David Milodowski <d.t.milodowski@ed.ac.uk>
2017-10-17 16:14:49 +0100

    Mexico layers

--- src/potentialAGB_Mexico_app.py
+++ src/potentialAGB_Mexico_app.py
@@ -21,50 +21,40 @@ plt.register_cmap(name='plasma', cmap=cmaps.plasma)
 plt.register_cmap(name='magma', cmap=cmaps.magma)
 plt.set_cmap(cmaps.viridis)
 
-NetCDF_file = '/home/dmilodow/DataStore_GCEL/AGB/AGBregpot.nc'
-SAVEDIR = './'
-ds,geoTrans = EO.load_NetCDF(NetCDF_file,lat_var = 'lat', lon_var = 'lon')
+DATADIR = '/disk/scratch/local.2/mexico_PFB/'
+SAVEDIR = '/home/dmilodow/DataStore_DTM/EOlaboratory/EOlab/MexicoPotentialAGB/'
+NetCDF_file = 'mexico_PFB.nc'
+
+ds,geoTrans = EO.load_NetCDF(DATADIR+NetCDF_file,lat_var = 'lat', lon_var = 'lon')
 resampling_scalar = 3.
-vars = ['AGBobs','AGBpot','AGBreg']
+vars = ['AGB-MEX','AGBpot','forests']
 dataset, geoTrans = EO.resample_dataset(ds,geoTrans,vars,resampling_scalar)
 
-cmaps = ['YlGn','YlGn','PRGn']
-ulims = [230.,230.,100.]
-llims = [0.,0.,-100.]
-#axis_labels = ['AGB$_{obs}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ - AGB$_{obs}$ / Mg(C) ha$^{-1}$']
-axis_labels = ['AGB$_{obs}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ / Mg(C) ha$^{-1}$', 'AGB deficit / Mg(C) ha$^{-1}$']
+dataset['AGBreg'] = dataset['AGBpot']-dataset['AGB-MEX']
+dataset['AGBreg'][dataset['AGB-MEX']==-9999] = -9999.
+
+dataset['refpot'] = dataset['AGBpot']-dataset['AGB-MEX']
+dataset['refpot'][dataset['AGB-MEX']==-9999] = -9999.
+dataset['refpot'][dataset['forests']==1] = 0.
+
+vars = ['AGB-MEX','AGBpot','AGBreg','refpot','forests']
+cmaps = ['viridis','viridis','PRGn','inferno','viridis']
+ulims = [230.,230.,100.,100.,1.]
+llims = [0.,0.,-100.,0.,0.]
+axis_labels = ['AGB$_{obs}$ / Mg(C) ha$^{-1}$', 'AGB$_{potential}$ / Mg(C) ha$^{-1}$', 'AGB deficit / Mg(C) ha$^{-1}$', 'Reforestation potential / Mg(C) ha$^{-1}$','Forest mask (1 = Forest)']
 
 for vv in range(0,len(vars)):
     print vars[vv]
-    file_prefix = SAVEDIR + 'tropics_' + vars[vv]
+    file_prefix = SAVEDIR + 'mexico_' + vars[vv]
 
     # delete existing dataset if present
-    if 'tropics_'+vars[vv]+'_data.tif' in os.listdir(SAVEDIR):
-        os.system("rm %s" % ('tropics_'+vars[vv]+'_data.tif'))
-    if 'tropics_'+vars[vv]+'_display.tif' in os.listdir(SAVEDIR):
-        os.system("rm %s" % ('tropics_'+vars[vv]+'_display.tif'))
-
-    #EO.write_array_to_data_layer_GeoTiff(dataset.variables[vars[vv]],geoTrans, file_prefix)
+    if 'mexico_'+vars[vv]+'_data.tif' in os.listdir(SAVEDIR):
+        os.system("rm %s" % ('mexico_'+vars[vv]+'_data.tif'))
+    if 'mexico_'+vars[vv]+'_display.tif' in os.listdir(SAVEDIR):
+        os.system("rm %s" % ('mexico_'+vars[vv]+'_display.tif'))
 
     EO.write_array_to_display_layer_GeoTiff(dataset[vars[vv]], geoTrans, file_prefix, cmaps[vv], ulims[vv], llims[vv])
     EO.plot_legend(cmaps[vv],ulims[vv],llims[vv],axis_labels[vv], file_prefix)
-
-"""
-# create additional layer which indicates potential biomass but thresholds at zero
-cmap='YlGnBu'
-ulim = 100
-llim = 0
-axis_label = 'reforestation potential / Mg(C) ha$^{-1}$'
-
-if 'tropics_reforest_potential_data.tif' in os.listdir(SAVEDIR):
-    os.system("rm %s" % ( 'tropics_reforest_potential_data.tif'))
-if 'tropics_reforest_potential_display.tif' in os.listdir(SAVEDIR):
-    os.system("rm %s" % ('tropics_reforest_potential_display.tif'))
-
-EO.write_array_to_display_layer_GeoTiff(dataset['AGBreg'], geoTrans, 'tropics_reforest_potential', cmap, ulim, llim)
-EO.plot_legend(cmap, ulim,llim,axis_label, 'tropics_reforest_potential')
-"""
-
 
 # Now write the quantitative display layers to file.  These probide the 
 rows, cols = dataset[vars[0]].shape
@@ -75,7 +65,7 @@ areas = geo.calculate_cell_area_array(latitude,longitude, area_scalar = 1./10.**
 # loop through the variables, multiplying by cell areas to give values in Mg
 for vv in range(0,len(vars)):
     print vars[vv]
-    file_prefix = SAVEDIR + 'tropics_' + vars[vv] + '_total_data'
+    file_prefix = SAVEDIR + 'mexico_' + vars[vv] + '_total_data'
 
     out_array = dataset[vars[vv]] * areas
     out_array[dataset[vars[vv]]==-9999]=-9999  # not sure why np.asarray step is needed but gets the job done
@@ -86,5 +76,5 @@ for vv in range(0,len(vars)):
 # nodata values
 areas_out = areas.copy()
 areas_out[np.asarray(dataset[vars[0]])==-9999] = -9999
-area_file_prefix = SAVEDIR + 'tropics_cell_areas_data'
+area_file_prefix = SAVEDIR + 'mexico_cell_areas_data'
 EO.write_array_to_data_layer_GeoTiff(areas_out, geoTrans, area_file_prefix)
