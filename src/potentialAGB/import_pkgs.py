ae3cafb0d2bb7b7d145b70ad1d4c27950e69fb9c
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-02-26 15:48:42 +0000

    removing unnecessary pckgs

--- src/potentialAGB/import_pkgs.py
+++ src/potentialAGB/import_pkgs.py
@@ -16,15 +16,9 @@ plt.cla()
 import xarray as xr
 import geopandas as geopandas
 from geopandas.tools import sjoin
-import cartopy as cartopy
-from cartopy import config as config
-import cartopy.crs as ccrs
 from cartopy.io.shapereader import Reader as Reader
-from cartopy.feature import ShapelyFeature as ShapelyFeature
 from shapely.geometry import Point as Point
 
 # custom libraries
 sys.path.append('../')
 import prepare_EOlab_layers as EO
-sys.path.append('/home/dmilodow/DataStore_DTM/FOREST2020/EOdata/EO_data_processing/src/')
-import auxilliary_functions as aux
