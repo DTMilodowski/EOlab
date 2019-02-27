1da65e3974a9c10bb412f2111f9adc4d8d3dc006
David Milodowski <d.t.milodowski@ed.ac.uk>
2019-02-27 10:21:33 +0000

    this script not currently required, but would be nice to use something along these lines for importing in future

--- src/potentialAGB/import_pkgs.py
+++ src/potentialAGB/import_pkgs.py
@@ -1,24 +1,25 @@
 # General libraries
-import numpy as np
-import pandas as pd
-import os as os
-import sys as sys
+def import_pkgs():
+    import numpy as np
+    import pandas as pd
+    import os as os
+    import sys as sys
 
-# Plotting libraries
-import matplotlib as mpl
-import matplotlib.cm as cm
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap as ListedColormap
-import seaborn as sns
-plt.cla()
+    # Plotting libraries
+    import matplotlib as mpl
+    import matplotlib.cm as cm
+    import matplotlib.pyplot as plt
+    from matplotlib.colors import ListedColormap as ListedColormap
+    import seaborn as sns
+    plt.cla()
 
-# GIS libraries
-import xarray as xr
-import geopandas as geopandas
-from geopandas.tools import sjoin
-from cartopy.io.shapereader import Reader as Reader
-from shapely.geometry import Point as Point
+    # GIS libraries
+    import xarray as xr
+    import geopandas as geopandas
+    from geopandas.tools import sjoin
+    from cartopy.io.shapereader import Reader as Reader
+    from shapely.geometry import Point as Point
 
-# custom libraries
-sys.path.append('../')
-import prepare_EOlab_layers as EO
+    # custom libraries
+    sys.path.append('../')
+    import prepare_EOlab_layers as EO
